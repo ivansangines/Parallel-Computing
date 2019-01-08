65701fdb646dacdf64d4e73df1dd3b1870a43003
ivansangines <35933947+ivansangines@users.noreply.github.com>
2019-01-08 12:10:08 -0500

    Practicing threads in C#

---
+++ Threads1/CrossThreading/CrossThreading/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CrossThreading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClock = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClock
+            // 
+            this.btnClock.Location = new System.Drawing.Point(114, 56);
+            this.btnClock.Name = "btnClock";
+            this.btnClock.Size = new System.Drawing.Size(75, 23);
+            this.btnClock.TabIndex = 0;
+            this.btnClock.Text = "Start_Clock";
+            this.btnClock.UseVisualStyleBackColor = true;
+            this.btnClock.Click += new System.EventHandler(this.btnClock_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTime.Location = new System.Drawing.Point(101, 134);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(100, 23);
+            this.lblTime.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnClock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClock;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ Threads1/Threading2/Threading2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Threading2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.HoursWorked = Convert.ToInt32(textBox1.Text);
+            emp.PayRate = Convert.ToDouble(textBox2.Text);
+            label3.Text = "The Employee salary is $ " + emp.ComputePay();
+
+        }
+    }
+}
